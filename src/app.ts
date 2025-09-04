72ec9f4640e84406d8fec6fe78b7ad2ee7db221c
SayarGyi007-lab <your-thss152021@gmail.com>
2025-09-04 17:08:07 +0700

    chg 2

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+// app.use(cors())
 app.use(cookieParser())
 app.use(UserRoute)
 app.use(PostRoute)
